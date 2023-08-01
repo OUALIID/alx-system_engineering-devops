0a3a20d3306ad7b02e74aa17f9c3167a917d1e98
OUALIID <walidelhadim8@gmail.com>
2023-08-01 18:53:23 +0100

    The code searches for occurrences of the pattern 'hbt', followed by one or more occurrences of the letter 't'

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -3,6 +3,9 @@
 The code searches for occurrences of the pattern 'hbt', followed by one or more occurrences of the letter 't',
 in the first command-line argument (ARGV[0]), and then it prints the matched substrings as an array.
 
+`+`: `+`: The plus sign (+) is a quantifier in regular expressions. It means 'one or more occurrences of the preceding character.'
+In this case, the preceding character is 't'.
+
                         ==> puts ARGV[0].scan(/hbt+n/).join <==
 The `join` method combines all the elements of the array(result of `scan`) into a single string, with no separator between them.
 """
