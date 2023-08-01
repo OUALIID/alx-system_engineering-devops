ba1589aba63ce459e4c45fb5c0ad0051813037f3
OUALIID <walidelhadim8@gmail.com>
2023-08-01 18:15:34 +0100

    Searches the input string for all occurrences of the exact word 'School'

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 """
-This code searches the input string for all occurrences of the word `school` exactly
-
-puts ARGV[0].scan(/School/).join
-
+This code searches the input string for all occurrences of the word `school` exactly.
+                    ==> puts ARGV[0].scan(/School/).join <==
 The `join` method combines all the elements of the array(result of `scan`) into a single string, with no separator between them.
 """
 puts ARGV[0].scan(/School/)
