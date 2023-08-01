07f38a5924cd55d518359bf9e8ad4336a43c8f50
OUALIID <walidelhadim8@gmail.com>
2023-08-01 18:35:36 +0100

    The code searches for occurrences of the pattern hbt, followed by one or more occurrences of the letter 't'

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 """
-The code searches for occurrences of the pattern "hbt", followed by one or more occurrences of the letter 't',
+The code searches for occurrences of the pattern 'hbt', followed by one or more occurrences of the letter 't',
 in the first command-line argument (ARGV[0]), and then it prints the matched substrings as an array.
 
-                        ==> puts ARGV[0].scan(/hb?t?n/).join <==
+                        ==> puts ARGV[0].scan(/hbt+n/).join <==
 The `join` method combines all the elements of the array(result of `scan`) into a single string, with no separator between them.
 """
 puts ARGV[0].scan(/hbt+n/)
