60ebbf054e4aac59d0ecfebdb34a96c307aa8525
OUALIID <walidelhadim8@gmail.com>
2023-08-01 18:21:16 +0100

    The code searches for occurrences of the pattern hbt followed by 2 to 5 occurrences of the letter 't' and ending with the letter 'n'

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env ruby
+"""
+The code searches for occurrences of the pattern `hbt` followed by 2 to 5 occurrences of the letter
+'t' and ending with the letter 'n' in the first command-line argument (ARGV[0]).
 
-puts ARGV[0].scan(/hbt{2,5}n/).join
+                        ==> puts ARGV[0].scan(/School/).join <==
+The `join` method combines all the elements of the array(result of `scan`) into a single string, with no separator between them.
+"""
+puts ARGV[0].scan(/hbt{2,5}n/)
