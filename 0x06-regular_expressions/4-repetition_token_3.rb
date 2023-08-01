b4e8e2a02717ded42a3f09e8fa7477dbb259addf
OUALIID <walidelhadim8@gmail.com>
2023-08-01 18:50:18 +0100

    The code searches for occurrences of the pattern hbt, followed by zero or more occurrences of the letter 't'

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
 """
-The code searches for occurrences of the pattern 'hb', followed by zero or more occurrences of the letter 't',
-and ending with the letter 'n' in the first command-line argument (ARGV[0]), and then it prints the matched substrings as an array.
+The code searches for occurrences of the pattern 'hbt', followed by zero or more occurrences of the letter 't', 
+in the first command-line argument (ARGV[0]), and then it prints the matched substrings as an array.
+
+`*`: The asterisk (*) is a special character in regular expressions known as the 'asterisk quantifier.'
+It means 'zero or more occurrences of the preceding character.' In this case, the preceding character is 't'.
 
                         ==> puts ARGV[0].scan(/hbt*n/).join <==
 The `join` method combines all the elements of the array(result of `scan`) into a single string, with no separator between them.
