fafeee938f3418026c8e593b7fc6d56ecb71e22a
OUALIID <walidelhadim8@gmail.com>
2023-08-01 18:26:10 +0100

    The code searches for occurrences of the pattern hb, followed by an optional 't', and ending with an optional 'n'

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/env ruby
+"""
+The code searches for occurrences of the pattern 'hb', followed by an optional 't',
+and ending with an optional 'n' in the first command-line argument (ARGV[0]),
+and then it prints the matched substrings as a single concatenated string without any separator.
 
-puts ARGV[0].scan(/hb?t?n/).join
+                        ==> puts ARGV[0].scan(/hb?t?n/).join <==
+The `join` method combines all the elements of the array(result of `scan`) into a single string, with no separator between them.
+"""
+puts ARGV[0].scan(/hb?t?n/)
