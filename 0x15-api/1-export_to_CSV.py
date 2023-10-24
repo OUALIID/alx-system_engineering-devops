b790e16c7ec688571810cc71ef2949dfaf393b42
OUALIID <walidelhadim8@gmail.com>
2023-10-24 03:46:20 +0100

    Export data in CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+"""Export data in CSV format."""
 
+if __name__ == "__main__":
+
+    import csv
+    import requests
+    import sys
+
+    id = int(sys.argv[1])
+    user_request = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{id}")
+    user = user_request.json()["username"]
+
+    tasks_request = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={id}")
+
+    filename = "2.csv"
+    for task in tasks_request.json():
+        if task['userId']:
+            with open(filename, mode="a") as file:
+                writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+                data = [
+                    str(id),
+                    str(user),
+                    str(task["completed"]),
+                    str(task["title"]),
+                ]
+                writer.writerow(data)
