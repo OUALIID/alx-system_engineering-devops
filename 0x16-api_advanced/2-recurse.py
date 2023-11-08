bb27146819dd0be936fc610ead9fb10881623949
OUALIID <walidelhadim8@gmail.com>
2023-11-08 19:27:40 +0100

    A recursive function that queries the Reddit API and returns a listcontaining the titles of all hot articles for a given subreddit.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,39 +12,26 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
     """
     if count >= 10:
         return hot_list
+    else:
+        params = {}
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     if after:
         params = {"after": after}
-    else:
-        params = {}  # Initialize an empty parameter dictionary
-
     response = requests.get(
         url,
         headers={"User-Agent": "custom"},
-        params=params  # Pass the query parameters here
+        params=params
     )
 
     if response.status_code == 200:
-        data = response.json()["data"]
-        children = data["children"]
-
-        if not children:
+        if not response.json()["data"]["children"]:
             return hot_list
 
-        for item in children:
+        for item in response.json()["data"]["children"]:
             hot_list.append(item["data"]["title"])
 
-        after = data["after"]
+        after = response.json()["data"]["after"]
         return recurse(subreddit, hot_list, after, count + 1)
 
-    return None  # Return None if the response status code is not 200
-
-# Example usage:
-if __name__ == '__main__':
-    subreddit = "programming"  # Replace with your desired subreddit
-    hot_articles = recurse(subreddit)
-    if hot_articles is not None:
-        print(len(hot_articles))
-    else:
-        print("None")
+    return None
