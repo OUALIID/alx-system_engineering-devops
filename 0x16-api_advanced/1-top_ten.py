84bc128d866be2b4d9fa4fd990b554c3705a6f73
OUALIID <walidelhadim8@gmail.com>
2023-11-08 11:58:26 +0100

    A function that queries the Reddit API and prints the titles of the first 10 top posts listed on a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,12 +6,17 @@ import requests
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    """
+    A function that queries the Reddit API and prints the titles
+    of the first 10 top posts listed on a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(
         url,
         headers={"User-Agent": "custom"}
     )
-    if requests == 200:
-        return response.json()["data"]["title"]
+    if response.status_code == 200:
+        for item in response.json()["data"]["children"]:
+            print(item["data"]["title"])
     else:
-        return 0
+        print(None)
