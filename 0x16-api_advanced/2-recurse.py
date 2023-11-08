2d1804aaf85af9bf9de7ff6e41e1cbf59ca51ca6
OUALIID <walidelhadim8@gmail.com>
2023-11-08 19:48:47 +0100

    A recursive function that queries the Reddit API and returns a listcontaining the titles of all hot articles for a given subreddit.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
 """
-0x16. API advanced
+0x16-api_advanced
 """
-
 import requests
 
-def recurse(subreddit, hot_list=[], after=None):
+
+def recurse(subreddit, hot_list=[], after=None, count=0):
     """
     A recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit.
     """
-    if not subreddit:
+    if not subreddit or not isinstance(subreddit, str):
         return None
+    if count >= 10:
+        return hot_list
+    else:
+        params = {}
 
-    params = {"after": after} if after else {}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after:
+        params = {"after": after}
     response = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/hot.json",
+        url,
         headers={"User-Agent": "custom"},
         params=params
     )
 
-    if response.status_code != 200:
-        return None
+    if response.status_code == 200:
+        if not response.json()["data"]["children"]:
+            return hot_list
 
-    data = response.json().get("data", {})
-    children = data.get("children", [])
+        for item in response.json()["data"]["children"]:
+            hot_list.append(item["data"]["title"])
 
-    hot_list.extend(item["data"]["title"] for item in children)
+        after = response.json()["data"]["after"]
+        return recurse(subreddit, hot_list, after, count + 1)
 
-    return recurse(subreddit, hot_list, after=data.get("after")) if data.get("after") else hot_list
+    return None
