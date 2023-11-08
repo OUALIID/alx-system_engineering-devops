89078cde5f45f4a549f90671d77e89c30ed807ea
OUALIID <walidelhadim8@gmail.com>
2023-11-08 20:10:31 +0100

    A recursive function that queries the Reddit API and returns a listcontaining the titles of all hot articles for a given subreddit.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,9 +6,9 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None, count=0):
-    """A recursive function that queries the Reddit
-    API and returns a list containing the titles
-    of all hot articles for a given subreddit."""
+    """A recursive function that queries the Reddit API and
+    returns a list containing the titles of all hot articles for
+    a given subreddit."""
     if count >= 10:
         return hot_list
     else:
