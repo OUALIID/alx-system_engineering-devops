31da103512eea5b2ef4ddaf9bece9926b25606f0
OUALIID <walidelhadim8@gmail.com>
2023-11-08 20:06:41 +0100

    A recursive function that queries the Reddit API and returns a listcontaining the titles of all hot articles for a given subreddit.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
 """
-0x16. API advanced
+0x16-api_advanced
 """
-
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=None, count=0):
     """A recursive function that queries the Reddit
     API and returns a list containing the titles of all
     hot articles for a given subreddit. If no results
     are found for the given subreddit, the function should
     return None."""
-    if not subreddit or not isinstance(subreddit, str):
-        return None
-    params = {"after": after} if after else {}
+    if count >= 10:
+        return hot_list
+    else:
+        params = {}
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after:
+        params = {"after": after}
     response = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "costum"},
-        params=params,
+        url,
+        headers={"User-Agent": "custom"},
+        params=params
     )
 
     if response.status_code == 200:
-        if response.json()["data"]:
-            data = response.json()["data"]
-            for item in data["children"]:
-                hot_list.append(item["data"]["title"])
-            if data["after"]:
-                return recurse(subreddit, hot_list, after=data["after"])
-            else:
-                return hot_list
-    else:
-        return None
+        if not response.json()["data"]["children"]:
+            return hot_list
+
+        for item in response.json()["data"]["children"]:
+            hot_list.append(item["data"]["title"])
+
+        after = response.json()["data"]["after"]
+        return recurse(subreddit, hot_list, after, count + 1)
+
+    return None
