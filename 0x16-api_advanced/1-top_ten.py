4718410c64690545a4d21e6237a8c50370293eb5
OUALIID <walidelhadim8@gmail.com>
2023-11-08 10:28:15 +0100

    A function that queries the Reddit API and returns the number of subscribers to a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+0x16-api_advanced
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(
+        url,
+        headers={"User-Agent": "custom"}
+    )
+    if requests == 200:
+        return response.json()["data"]["title"]
+    else:
+        return 0
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""
+0x16-api_advanced
+"""
+
+
